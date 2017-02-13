f82b0e81e2c6f87991621d5b50ada8432d5f51ea
valerio <valerio@basili.co>
2017-02-13 15:58:42 +0100

    gulp and bower env prepared

--- gulpfile.js
+++ gulpfile.js
@@ -144,6 +144,17 @@ gulp.task('build:vendors-js', function() {
 
 });
 
+gulp.task('build:vendors-css', function() {
+  var vendors;
+
+  if (paths().builds.css.vendors) {
+    vendors = gulp.src(paths().builds.css.vendors)
+      .pipe(concatCss('vendors.min.css', { rebaseUrls: false }))
+      .pipe(gulp.dest('source/css'))
+      .pipe(plugins.notify({ message: 'CSS: vendors minified', onLast: true }));
+  }
+});
+
 // CSS Copy
 gulp.task('copy:css', function(){
   return gulp.src(path.resolve(paths().source.css, '*.css'))
