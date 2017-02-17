f4ac4c277278c186fd355dba03eb7ac5126368e6
valerio <valerio@basili.co>
2017-02-17 17:35:31 +0100

    create card carousel

--- source/_src/js/scripts.js
+++ source/_src/js/scripts.js
@@ -1,17 +1,25 @@
 (function($, device, Swiper) {
-  var $cardsCarousel = $('.card-list.card-list--carousel');
+  var $cardsCarousel = $('.card-carousel');
 
   $cardsCarousel.each(function() {
     $(this)
       .find('.swiper-container')
       .swiper({
-        speed: 600,
         spaceBetween: 20,
+        speed: 800,
         simulateTouch: false,
-        pagination: "",
-        paginationType: "bullet", // fraction
-        nextButton: "",
-        prevButton: "",
+        pagination: (device.mobile() && !device.tablet()) ? $(this).find('.carousel__pagination') : $(this).find('.carousel__fraction'),
+        paginationType: (device.mobile() && !device.tablet()) ? "bullets" : "fraction",
+        paginationHide: false,
+        nextButton: $(this).find('.carousel__next'),
+        prevButton: $(this).find('.carousel__prev'),
+        onInit: function() {
+          if ($current.find('.swiper-slide').length === 1) {
+            $current
+              .find('.carousel__pagination, .carousel__nav')
+              .remove();
+          }
+        },
         breakpoints: {
           480:  { slidesPerView: 1 },
           790:  { slidesPerView: 2 },
