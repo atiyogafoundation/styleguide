cc5dcda0dfc8d6561bb141c2bbb0452bf2f48c31
valerio <valerio@basili.co>
2017-02-17 17:14:47 +0100

    create carousel

--- source/_src/js/scripts.js
+++ source/_src/js/scripts.js
@@ -1,3 +1,52 @@
-(function($) {
+(function($, device, Swiper) {
+  var $cardsCarousel = $('.card-list.card-list--carousel');
 
-})(jQuery);
+  $cardsCarousel.each(function() {
+    $(this)
+      .find('.swiper-container')
+      .swiper({
+        speed: 600,
+        spaceBetween: 20,
+        simulateTouch: false,
+        pagination: "",
+        paginationType: "bullet", // fraction
+        nextButton: "",
+        prevButton: "",
+        breakpoints: {
+          480:  { slidesPerView: 1 },
+          790:  { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1920: {
+            slidesPerView: 4,
+            slidesPerGroup: 2
+          }
+        }
+      });
+  });
+
+  var $carousels = $('.carousel');
+
+  $carousels.each(function() {
+    var $current = $(this);
+
+    $current
+      .find('.swiper-container')
+      .swiper({
+        speed: 800,
+        simulateTouch: false,
+        pagination: (device.mobile() && !device.tablet()) ? $(this).find('.carousel__pagination') : $(this).find('.carousel__fraction'),
+        paginationType: (device.mobile() && !device.tablet()) ? "bullets" : "fraction",
+        paginationHide: false,
+        nextButton: $(this).find('.carousel__next'),
+        prevButton: $(this).find('.carousel__prev'),
+        onInit: function() {
+          if ($current.find('.swiper__slide').length === 1) {
+            $$current
+              .find('.carousel__pagination, .carousel__nav')
+              .remove();
+          }
+        }
+      });
+  });
+
+})(jQuery, device, Swiper);
